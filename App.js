6a5fb79d41b5f16b0d9e4823a242c4428b0b91b8
Mina-Mikhael <mina.s.mikhael@gmail.com>
2023-12-20 01:27:56 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
+import MyComp from "./src/MyComp";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>HELLO WORLD!!</Text>
+      <Text style={styles.text2}>I'm happy to use React Native</Text>
+      <Text style={styles.text2}>I'm happy to use React Native</Text>
+      <MyComp />
+      <Button
+        buttonStyle={styles.text2}
+        title="Click Me!"
+        onPress={() => alert("I'm happy to use React Native")}
+      />
     </View>
   );
 }
@@ -13,8 +20,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#08395a",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 20,
+    color: "white",
+  },
+  text: {
+    color: "white",
+    fontSize: 50,
+  },
+  text2: {
+    color: "#95efeb",
+    fontSize: 30,
+    textAlign: "center",
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: "#9befeb",
+    borderRadius: 10,
+    backgroundColor: "#276d9c",
   },
 });
